c7e75346939d4dbda7867d3493fa5b9309a44b11
ertix222 <ertix222@gmail.com>
2025-01-13 09:39:46 +0100

    rename to the write name

---
+++ debugging/factorial.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+if len(sys.argv) == 1:
+    print("Aucun argument fourni.")
+else:
+    print("Liste des arguments :")
+    for i, arg in enumerate(sys.argv):
+        print(f"Argument {i}: {arg}")
+        
