3898ce1939358f097fd3d0631e2611b19c78f071
ertix222 <ertix222@gmail.com>
2025-01-13 09:54:03 +0100

    t1 chat

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-# Check if any arguments are provided
-if len(sys.argv) == 1:
-    print("No arguments provided.")
-else:
-    print("Arguments passed to the script:")
-    for i, arg in enumerate(sys.argv):
-        print(f"Argument {i}: {arg}")
+# Start from index 1 to exclude the script name
+for arg in sys.argv[1:]:
+    print(arg)
